6a19fa22b0632cb036b424e076916b1c1edcfb33
malelilu <till.kossbach@docc.techstarter.de>
2023-01-12 14:55:46 +0100

    Hausaufgabe

--- hey.py
+++ hey.py
@@ -1,12 +1,22 @@
 import json
  #Öffne die Datei
-with open('accounts.json', 'r') as f:
+with open('accounts.json', 'r') as filejson:
         # Lade die JSON-Daten aus der Datei
-        data = json.load(f)
+        data = json.load(filejson)
 
-print(data)
+eingabe = input('was moechtes du eingeben?\noption a: alter\noption b: kontostand')
 
-for account in data:
- 
-     print("der Name ist:", account["name"])
- 
+if eingabe == "alter":
+ for account in data:
+        if int(account["age"]) >25:
+               print("alter:", account["age"], "name:", account["name"])
+
+# Höchsten Kontostand
+if eingabe == "kontostand":
+    summe = 0
+    for accounts in data:
+        pruf = accounts["balance"].replace(",","")
+        pruf = float(pruf)
+        if summe < pruf:
+                summe = pruf
+    print(summe)
