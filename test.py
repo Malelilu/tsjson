b34531b266d9a95b09b663d138c882794665c8ee
malelilu <till.kossbach@docc.techstarter.de>
2023-01-12 15:39:50 +0100

    Hausaufgabe update 3

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-hh
+import json
+ #Öffne die Datei
+with open('accounts.json', 'r') as filejson:
+        # Lade die JSON-Daten aus der Datei
+        data = json.load(filejson)
+
+eingabe = input('was moechtes du eingeben?\noption a: alter\noption b: kontostand\n')
+
+if eingabe == "alter":
+ for account in data:
+        if int(account["age"]) >25:
+               print("alter:", account["age"], "name:", account["name"])
+
+# Höchsten Kontostand
+if eingabe == "kontostand":
+    summe = 0
+    for accounts in data:
+        pruf = accounts["balance"].replace(",","")
+        pruf = float(pruf)
+        if summe < pruf:
+                summe = pruf
+    print(summe)
